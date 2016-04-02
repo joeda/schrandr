737f19e2a7f17be4ed77b356781467edbd874275
Johannes Janosovits <johannes@walnutempire.de>
2016-04-02 14:23:37 +0200

    Added arguments

--- schrandr.cpp
+++ schrandr.cpp
@@ -16,71 +16,71 @@
 #include "schrandr.h"
 
 namespace schrandr {
-
-std::unique_ptr<PIDFileHolder> pid_file_holder;
-std::atomic<int> interruption(0);
-unsigned int loop_duration(500000);
-
-PIDFileHolder::PIDFileHolder(unsigned int pid)
-: pid_file_(PID_FILE, std::ios_base::in)
-{
-    if (!pid_file_.fail()) {
-        std::cout << "PID file exists?" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-    pid_file_.close();
-    pid_file_.open(PID_FILE, std::ios_base::out | std::ios_base::trunc);
-    pid_file_.exceptions(pid_file_.badbit | pid_file_.failbit);
-    pid_file_ << pid << std::flush;
-}
-
-
-PIDFileHolder::~PIDFileHolder()
-{
-    pid_file_.close();
-    std::cout << "Now Closing" << std::endl;
-    if (unlink(PID_FILE) == -1) {
-        exit(EXIT_FAILURE);
-      }
-    std::cout << "Unlinking worked apparently." << std::endl;
-}
-
-void sig_handler(int signum) {
-    switch(signum) {
-        case SIGHUP:
-        case SIGINT:
-        case SIGTERM:
-            interruption = signum;
-            break;
-        case SIGUSR1:
-            break;
-        case SIGSEGV:
-        // Let's hope memory isn't too fucked up to get through with this ;)
-            break;
-        case SIGUSR2:
-            interruption = signum;
-            break;
+    
+    std::unique_ptr<PIDFileHolder> pid_file_holder;
+    std::atomic<int> interruption(0);
+    unsigned int loop_duration(500000);
+    
+    PIDFileHolder::PIDFileHolder(unsigned int pid)
+    : pid_file_(PID_FILE, std::ios_base::in)
+    {
+        if (!pid_file_.fail()) {
+            std::cout << "PID file exists?" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        pid_file_.close();
+        pid_file_.open(PID_FILE, std::ios_base::out | std::ios_base::trunc);
+        pid_file_.exceptions(pid_file_.badbit | pid_file_.failbit);
+        pid_file_ << pid << std::flush;
+    }
+    
+    
+    PIDFileHolder::~PIDFileHolder()
+    {
+        pid_file_.close();
+        std::cout << "Now Closing" << std::endl;
+        if (unlink(PID_FILE) == -1) {
+            exit(EXIT_FAILURE);
+        }
+        std::cout << "Unlinking worked apparently." << std::endl;
+    }
+    
+    void sig_handler(int signum) {
+        switch(signum) {
+            case SIGHUP:
+            case SIGINT:
+            case SIGTERM:
+                interruption = signum;
+                break;
+            case SIGUSR1:
+                break;
+            case SIGSEGV:
+                // Let's hope memory isn't too fucked up to get through with this ;)
+                break;
+            case SIGUSR2:
+                interruption = signum;
+                break;
+        }
+    }
+    
+    
+    void handle_uncaught()
+    {
+        std::cout << "Panic!" << std::endl;
     }
 }
 
-
-void handle_uncaught()
-{
-    std::cout << "Panic!" << std::endl;
-}
-}
-
-int main(void)
+int main(int argc, char **argv)
 {
     using namespace schrandr;
-
+    
     struct sigaction handler;
-
+    
     std::set_terminate(handle_uncaught);
-
+    
     memset(&handler, 0, sizeof(handler));
     handler.sa_handler = sig_handler;
-
+    
     // Install signal handler only after FanControl object has been created
     // since it is used by the handler.
     if (sigaction(SIGHUP, &handler, NULL)
@@ -89,9 +89,47 @@ int main(void)
         || sigaction(SIGUSR1, &handler, NULL)
         || sigaction(SIGUSR2, &handler, NULL)
         || sigaction(SIGSEGV, &handler, NULL)) {
-            return 1;
+        return 1;
+        }
+        int aflag = 0;
+    int bflag = 0;
+    char *cvalue = NULL;
+    int index;
+    int c;
+    
+    opterr = 0;
+    while ((c = getopt (argc, argv, "abc:")) != -1) {
+        switch (c)
+        {
+            case 'a':
+                aflag = 1;
+                break;
+            case 'b':
+                bflag = 1;
+                break;
+            case 'c':
+                cvalue = optarg;
+                break;
+            case '?':
+                if (optopt == 'c')
+                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf (stderr,
+                             "Unknown option character `\\x%x'.\n",
+                             optopt);
+                    return 1;
+            default:
+                abort ();
+        }
     }
-
+    printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+            aflag, bflag, cvalue);
+    
+    for (index = optind; index < argc; index++)
+        printf ("Non-option argument %s\n", argv[index]);
+    
     pid_t pID = fork();
     if (pID == 0)                                           // child
     {
@@ -112,12 +150,12 @@ int main(void)
     {
         std::cerr << "Failed to fork" << std::endl;
         exit(EXIT_FAILURE);
-    // Throw exception
+        // Throw exception
     }
     else                                                    // parent
     {
         // Code only executed by parent process
         std::cout << "I'm the parent process! Goodbye!" << std::endl;  
     }
-
+    
 }
